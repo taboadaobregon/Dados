04618b33182fc303e912a1e9643f0d626ce06646
unknown <taboadaobhuaura@gmail.com>
2021-12-12 14:12:04 -0500

    Dado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzquierda = '../assets/img/dice4.png';
+  dadoDerecha = '../assets/img/dice1.png';
+
+  numero1 = 1;
+  numero2 = 2;
+
+  tirarDados(): void{
+
+    this.numero1 = Math.round( Math.random() * 5) + 1;
+    this.numero2 = Math.round( Math.random() * 6) + 1;
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+    
+
+  }
 }
